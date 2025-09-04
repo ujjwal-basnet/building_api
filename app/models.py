fbb3d656e5730723135417457807a964787f71a0
ujjwal <ujjwalvictus15@gmail.com>
2025-09-04 10:15:05 +0545

    feat(db): add database config, initial models, and dataset structure

---
+++ app/models.py
@@ -0,0 +1,3 @@
+"""Defines the SQLAlchemy classe realted to the database tables """
+
+from  database import Base
---
+++ database/__init__.py
@@ -0,0 +1,43 @@
+"""
+Database configuration module.
+- Establishes a connection to the SQLite database with the correct settings.
+- Creates a session factory (`SessionLocal`) to manage database sessions.
+- Defines a declarative base class (`Base`) for all ORM models to inherit from.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+"""
+SQLite database URL
+- "sqlite" → database type (tells SQLAlchemy which database backend to use)
+- "./fantasy_data.db" → relative path to the database file
+"""
+SQLALCHEMY_DATABASE_URL = "sqlite:///./fantasy_data.db"
+
+
+"""
+Engine:
+- Core interface to the database
+- `check_same_thread=False` → allows multiple threads to use the same connection safely
+"""
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+
+"""
+Session factory:
+- autocommit=False → changes must be explicitly committed
+- autoflush=False → avoids automatic writes until commit
+- bind=engine → links session to our engine
+"""
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+"""
+Base class for ORM models:
+- All table classes (e.g., Player, Team) will inherit from this
+"""
+Base = declarative_base()
